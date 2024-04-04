fdea488be921a31228869e858cd73964a57d22d5
seddonnguyen <tri.seddon.nguyen@gmail.com>
2024-04-04 15:44:41 -0600

    Add Class Lambda Exercise

--- src/module/_303/hands_on_activity/lesson_14/Calc.java
+++ src/module/_303/hands_on_activity/lesson_14/Calc.java
@@ -1,6 +1,6 @@
 package module._303.hands_on_activity.lesson_14;
 
 @FunctionalInterface
-public interface Calc<T> {
-    T compute(T[] values);
+public interface Calc<E> {
+    E compute(E[] values);
 }
